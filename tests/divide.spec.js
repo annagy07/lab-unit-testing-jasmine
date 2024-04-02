1f21a51e028c04ceb54cb970c22a52fa769731ac
annahrtkpf <anna-hartkopf@gmx.de>
2024-04-02 16:47:55 +0200

    Solved lab

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -12,3 +12,32 @@ describe("Iteration 2", () => {
 })
 
 */
+
+describe("Iteration 2 | Divide Function", () => {
+  describe("Function divide", () => {
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
+
+    it("should take two arguments", () => {
+      expect(divide.length).toBe(2);
+    });
+
+    it("should return the quotient of the two numbers", () => {
+      expect(divide(10, 5)).toEqual(2);
+      expect(divide(15, 5)).toEqual(3);
+      expect(divide(100, 2)).toEqual(50);
+    });
+
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(divide(1)).toEqual(undefined);
+      expect(divide()).toEqual(undefined);
+      expect(divide(undefined, 1)).toEqual(undefined);
+    });
+    it("should return undefined if any of the two arguments is not a number.", () => {
+      expect(divide(1, "hi")).toEqual(undefined);
+      expect(divide(1, true)).toEqual(undefined);
+      expect(divide(undefined, 1)).toEqual(undefined);
+    });
+  });
+});
